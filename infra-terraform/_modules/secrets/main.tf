a4522772308c3c985fd2208de0557e64f31f46b1
David Wright <david.wright@smoothstack.com>
2022-03-12 10:58:17 -0600

    Change window to 7

--- infra-terraform/_modules/secrets/main.tf
+++ infra-terraform/_modules/secrets/main.tf
@@ -1,6 +1,6 @@
 resource "aws_secretsmanager_secret" "secret" {
   name_prefix             = var.secret_key
-  recovery_window_in_days = 5
+  recovery_window_in_days = 7
   tags                    = var.tags
 }
 
