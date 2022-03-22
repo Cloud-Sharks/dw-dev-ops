28b667398e471ef9c298d9b098c91e4ecbee929e
davidsmoothstack <93416047+davidsmoothstack@users.noreply.github.com>
2022-03-22 12:43:45 -0500

    Swap subnet elb tags (#17)

--- infra-terraform/dev/main.tf
+++ infra-terraform/dev/main.tf
@@ -20,14 +20,14 @@ module "vpc" {
   azs             = var.azs
 
   private_subnet_tags = merge({
+    "kubernetes.io/role/internal-elb" : 1
     "kubernetes.io/cluster/${var.eks_cluster_name}" : "owned",
-    "kubernetes.io/role/elb" : 1
     },
     local.global_tags
   )
 
   public_subnet_tags = merge({
-    "kubernetes.io/role/internal-elb" : 1
+    "kubernetes.io/role/elb" : 1
     },
     local.global_tags
   )
