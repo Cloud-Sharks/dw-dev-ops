96b26061d5e5edea7e5afeb68bfb3ae0f5337a49
David Wright <david.wright@smoothstack.com>
2022-05-03 13:54:27 -0500

    Add initial test

---
+++ terratest/terratest_test.go
@@ -0,0 +1,28 @@
+package terratest
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformHelloWorldExample(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../infra-terraform/dev",
+	})
+
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+
+	// Clean up resources with "terraform destroy" at the end of the test.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
+	output := terraform.Output(t, terraformOptions, "hello_world")
+	assert.Equal(t, "Hello, World!", output)
+}
