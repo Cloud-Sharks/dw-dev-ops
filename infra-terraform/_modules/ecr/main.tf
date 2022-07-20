03aad763a45cee0192a906dbb42615c2acd3587d
David Wright <david.wright@smoothstack.com>
2022-07-20 09:29:54 -0500

    Make images mutable

--- infra-terraform/_modules/ecr/main.tf
+++ infra-terraform/_modules/ecr/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "repo" {
   for_each             = var.repositories
   name                 = each.value
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
 }
 
 resource "aws_ecr_repository_policy" "policy" {
