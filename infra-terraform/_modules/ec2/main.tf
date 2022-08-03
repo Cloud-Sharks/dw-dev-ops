08e747d1d8290a26ad39de689aadd0b98b41368d
David Wright <david.wright@smoothstack.com>
2022-08-03 13:30:19 -0500

    Add ec2 module

---
+++ infra-terraform/_modules/ec2/main.tf
@@ -0,0 +1,70 @@
+################################################################################
+# Security Groups
+################################################################################
+
+resource "aws_security_group" "groups" {
+  for_each = var.ec2_configs
+
+  name   = each.value.name
+  vpc_id = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = each.value.ports
+    iterator = ingress
+
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge({ Name = each.value.name }, var.tags)
+}
+
+################################################################################
+# Instance AMI
+################################################################################
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+################################################################################
+# Instance
+################################################################################
+
+resource "aws_instance" "instances" {
+  for_each        = var.ec2_configs
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = each.value.instance_type
+  subnet_id       = var.public_subnet_id
+  security_groups = [for g in aws_security_group.groups : g.id if g.tags_all.Name == each.value.name]
+
+  root_block_device {
+    volume_size = each.value.volume_size
+  }
+
+  tags = merge({ Name = each.value.name }, var.tags)
+}
---
+++ infra-terraform/_modules/ec2/variables.tf
@@ -0,0 +1,24 @@
+variable "ec2_configs" {
+  type = map(object({
+    name          = string
+    ports         = set(number)
+    instance_type = string
+    is_public     = bool
+    volume_size   = number
+  }))
+}
+
+variable "public_subnet_id" {
+  type        = string
+  description = "Public Subnet"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "Name of the VPC to create the security groups in"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
