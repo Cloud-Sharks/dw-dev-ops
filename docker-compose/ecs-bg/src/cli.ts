d46c724f7a4ddb38a3fec4127e2e5d1bfbb60ff0
David Wright <david.wright@smoothstack.com>
2022-08-15 13:00:00 -0500

    Split cli from automation

---
+++ docker-compose/ecs-bg/src/cli.ts
@@ -0,0 +1,40 @@
+import {
+    DestroyResult,
+    InlineProgramArgs,
+    LocalWorkspace,
+    UpResult,
+} from "@pulumi/pulumi/automation";
+import { createCluster, createService } from "./ecs-cluster";
+
+const process = require("process");
+const [, , ...args] = process.argv;
+
+let destroy = false;
+
+if (args?.length > 0 && args[0]) {
+    if (args[0] === "destroy") destroy = true;
+}
+
+const run = async () => {
+    // Create our stack
+    const args: InlineProgramArgs = {
+        stackName: "ecs-bg-node",
+        projectName: "ecs-bg-node",
+        program: createCluster,
+    };
+
+    const stack = await LocalWorkspace.createOrSelectStack(args);
+
+    let result: DestroyResult | UpResult | undefined = undefined;
+
+    if (destroy) {
+        result = await stack.destroy({ onOutput: console.info });
+    } else {
+        result = await stack.up({ onOutput: console.info });
+    }
+
+    const summary = JSON.stringify(result?.summary.resourceChanges, null, 4);
+    console.log(`update summary: \n${summary}`);
+};
+
+run().catch((err) => console.log(err));
--- docker-compose/ecs-bg/src/ecs-cluster.ts
+++ docker-compose/ecs-bg/src/ecs-cluster.ts
@@ -12,7 +12,7 @@ interface ServiceConfig {
     executionRole: aws.iam.Role;
 }
 
-export default async function createCluster() {
+export async function createCluster() {
     // create a cluster
     const cluster = new aws.ecs.Cluster("dw-ecs-cluster", {
         name: "dw-ecs-cluster",
@@ -38,170 +38,163 @@ export default async function createCluster() {
         executionRole,
         listener,
     };
+}
 
-    createService(Service.Bank, Deployment.Blue, createServiceConfig);
-    createService(Service.Bank, Deployment.Green, createServiceConfig);
+export function createService(
+    service: Service,
+    deployment: Deployment,
+    config: ServiceConfig,
+): aws.ecs.Service {
+    const serviceName = `${service}-${deployment}`;
 
-    function createService(
-        service: Service,
-        deployment: Deployment,
-        config: ServiceConfig,
-    ): aws.ecs.Service {
-        const serviceName = `${service}-${deployment}`;
+    // target group for port 80
+    const serviceTg = new aws.lb.TargetGroup(serviceName, {
+        port: 80,
+        protocol: "HTTP",
+        targetType: "ip",
+        vpcId: config.vpcId,
+    });
 
-        // target group for port 80
-        const serviceTg = new aws.lb.TargetGroup(serviceName, {
-            port: 80,
-            protocol: "HTTP",
-            targetType: "ip",
-            vpcId: config.vpcId,
-        });
-
-        const listenerRule = new aws.lb.ListenerRule(serviceName, {
-            listenerArn: config.listener.arn,
-            actions: [
-                {
-                    type: "forward",
-                    targetGroupArn: serviceTg.arn,
-                },
-            ],
-            conditions: [
-                {
-                    pathPattern: {
-                        values: [`/${serviceName}`],
-                    },
-                },
-            ],
-        });
-
-        const taskDefinition = new aws.ecs.TaskDefinition(serviceName, {
-            family: serviceName,
-            cpu: "256",
-            memory: "512",
-            networkMode: "awsvpc",
-            requiresCompatibilities: ["FARGATE"],
-            executionRoleArn: config.executionRole.arn,
-            containerDefinitions: JSON.stringify([
-                {
-                    name: serviceName,
-                    image: "nginx",
-                    portMappings: [
-                        {
-                            containerPort: 80,
-                            hostPort: 80,
-                            protocol: "tcp",
-                        },
-                    ],
-                },
-            ]),
-        });
-
-        const svc = new aws.ecs.Service(serviceName, {
-            cluster: config.cluster.arn,
-            desiredCount: 1,
-            launchType: "FARGATE",
-            taskDefinition: taskDefinition.arn,
-            networkConfiguration: {
-                assignPublicIp: true,
-                subnets: getSubnets(config.vpcId).ids,
-                securityGroups: config.securityGroups.map((s) => s.id),
+    const listenerRule = new aws.lb.ListenerRule(serviceName, {
+        listenerArn: config.listener.arn,
+        actions: [
+            {
+                type: "forward",
+                targetGroupArn: serviceTg.arn,
             },
-            loadBalancers: [
-                {
-                    targetGroupArn: serviceTg.arn,
-                    containerName: serviceName,
-                    containerPort: 80,
+        ],
+        conditions: [
+            {
+                pathPattern: {
+                    values: [`/${serviceName}`],
                 },
-            ],
-        });
+            },
+        ],
+    });
 
-        return svc;
-    }
-
-    function generateSecurityGroup(
-        vpcId: Output<string>,
-    ): aws.ec2.SecurityGroup {
-        return new aws.ec2.SecurityGroup("example", {
-            vpcId: vpcId,
-            description: "HTTP access",
-            ingress: [
-                {
-                    protocol: "tcp",
-                    fromPort: 80,
-                    toPort: 80,
-                    cidrBlocks: ["0.0.0.0/0"],
-                },
-            ],
-            egress: [
-                {
-                    protocol: "-1",
-                    fromPort: 0,
-                    toPort: 0,
-                    cidrBlocks: ["0.0.0.0/0"],
-                },
-            ],
-        });
-    }
-
-    function createLoadBalancer(
-        name: string,
-        securityGroups: aws.ec2.SecurityGroup[],
-        subnets: Output<GetSubnetsResult>,
-    ): aws.lb.LoadBalancer {
-        return new aws.lb.LoadBalancer(name, {
-            name,
-            securityGroups: securityGroups.map((g) => g.id),
-            subnets: subnets.ids,
-        });
-    }
-
-    function createListener(
-        name: string,
-        port: number,
-        loadBalancer: aws.lb.LoadBalancer,
-    ) {
-        return new aws.lb.Listener(name, {
-            loadBalancerArn: loadBalancer.arn,
-            port,
-            defaultActions: [
-                {
-                    type: "fixed-response",
-                    fixedResponse: {
-                        statusCode: "404",
-                        contentType: "text/plain",
-                        messageBody: "Content not found",
+    const taskDefinition = new aws.ecs.TaskDefinition(serviceName, {
+        family: serviceName,
+        cpu: "256",
+        memory: "512",
+        networkMode: "awsvpc",
+        requiresCompatibilities: ["FARGATE"],
+        executionRoleArn: config.executionRole.arn,
+        containerDefinitions: JSON.stringify([
+            {
+                name: serviceName,
+                image: "nginx",
+                portMappings: [
+                    {
+                        containerPort: 80,
+                        hostPort: 80,
+                        protocol: "tcp",
                     },
+                ],
+            },
+        ]),
+    });
+
+    const svc = new aws.ecs.Service(serviceName, {
+        cluster: config.cluster.arn,
+        desiredCount: 1,
+        launchType: "FARGATE",
+        taskDefinition: taskDefinition.arn,
+        networkConfiguration: {
+            assignPublicIp: true,
+            subnets: getSubnets(config.vpcId).ids,
+            securityGroups: config.securityGroups.map((s) => s.id),
+        },
+        loadBalancers: [
+            {
+                targetGroupArn: serviceTg.arn,
+                containerName: serviceName,
+                containerPort: 80,
+            },
+        ],
+    });
+
+    return svc;
+}
+
+function generateSecurityGroup(vpcId: Output<string>): aws.ec2.SecurityGroup {
+    return new aws.ec2.SecurityGroup("dw-ecs-security-group", {
+        vpcId: vpcId,
+        description: "HTTP access",
+        ingress: [
+            {
+                protocol: "tcp",
+                fromPort: 80,
+                toPort: 80,
+                cidrBlocks: ["0.0.0.0/0"],
+            },
+        ],
+        egress: [
+            {
+                protocol: "-1",
+                fromPort: 0,
+                toPort: 0,
+                cidrBlocks: ["0.0.0.0/0"],
+            },
+        ],
+    });
+}
+
+function createLoadBalancer(
+    name: string,
+    securityGroups: aws.ec2.SecurityGroup[],
+    subnets: Output<GetSubnetsResult>,
+): aws.lb.LoadBalancer {
+    return new aws.lb.LoadBalancer(name, {
+        name,
+        securityGroups: securityGroups.map((g) => g.id),
+        subnets: subnets.ids,
+    });
+}
+
+function createListener(
+    name: string,
+    port: number,
+    loadBalancer: aws.lb.LoadBalancer,
+) {
+    return new aws.lb.Listener(name, {
+        loadBalancerArn: loadBalancer.arn,
+        port,
+        defaultActions: [
+            {
+                type: "fixed-response",
+                fixedResponse: {
+                    statusCode: "404",
+                    contentType: "text/plain",
+                    messageBody: "Content not found",
                 },
-            ],
-        });
-    }
+            },
+        ],
+    });
+}
 
-    function generateExecutionRole(): aws.iam.Role {
-        const role = new aws.iam.Role("example", {
-            assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
-                Service: "ecs-tasks.amazonaws.com",
-            }),
-        });
+function generateExecutionRole(): aws.iam.Role {
+    const role = new aws.iam.Role("ecs-exexution-role", {
+        assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+            Service: "ecs-tasks.amazonaws.com",
+        }),
+    });
 
-        new aws.iam.RolePolicyAttachment("example", {
-            role: role.name,
-            policyArn:
-                "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
-        });
+    new aws.iam.RolePolicyAttachment("ecs-exexution-role-attachment", {
+        role: role.name,
+        policyArn:
+            "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+    });
 
-        return role;
-    }
+    return role;
+}
 
-    function getSubnets(
-        vpcId: Output<string>,
-    ): Output<aws.ec2.GetSubnetsResult> {
-        return aws.ec2.getSubnetsOutput({
-            filters: [
-                {
-                    name: "vpc-id",
-                    values: [vpcId],
-                },
-            ],
-        });
-    }
+function getSubnets(vpcId: Output<string>): Output<aws.ec2.GetSubnetsResult> {
+    return aws.ec2.getSubnetsOutput({
+        filters: [
+            {
+                name: "vpc-id",
+                values: [vpcId],
+            },
+        ],
+    });
 }
