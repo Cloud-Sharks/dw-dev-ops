856d5b88862f9b5d019c8dbcdaf764a177f24858
David Wright <david.wright@smoothstack.com>
2022-08-17 14:45:12 -0500

    Add logs

--- docker-compose/ecs-bg/src/ecs-cluster.ts
+++ docker-compose/ecs-bg/src/ecs-cluster.ts
@@ -98,6 +98,15 @@ export const createService = (
                     },
                 ],
                 environment: env,
+                logConfiguration: {
+                    logDriver: "awslogs",
+                    options: {
+                        "awslogs-create-group": "true",
+                        "awslogs-group": serviceName,
+                        "awslogs-region": "us-east-1",
+                        "awslogs-stream-prefix": "dw-ecs",
+                    },
+                },
             },
         ]),
     });
